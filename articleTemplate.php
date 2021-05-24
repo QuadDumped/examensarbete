95e5c35fe664d9e19d920232aed31821c8c22e85
AlvinLarsson <42584015+QuadDumped@users.noreply.github.com>
2021-05-24 14:16:39 +0200

    Add files via upload

--- articleTemplate.php
+++ articleTemplate.php
@@ -1,12 +1,12 @@
 <html>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="styles.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <html lang="sv">
 
 <head>
-<div class="header">
-<p class="headertext">Das Reich Schweden Nyheter</p>
-<p class="headertext"></p>
+<div class="titleDiv">
+<p class="title">News Bot</p>
 </div>
 </head>
 
@@ -14,50 +14,25 @@
 <body>
 <div class="main">
 <?php
+
 $headertext = $_GET['headertext'];
 $image = $_GET['image'];
-$header = "<p class=\"headertext\">$headertext</p>";
+$text = $_GET['text'];
+$header = "<p class=headertext>$headertext</p>";
 $headerimage = "<img src=". $image . " height=300px width=600px><br>";
+$textBlock = "<p class=text>". $text . "</p>";
 
 echo $header;
 echo $headerimage;
+echo $textBlock;
+
 ?>
+
+
 </div>
 </body>
 
 
 
-<style>
 
- .main {
-  margin-left: 6%;
- }
-
- .headertext {
- font-size: 35;
- margin-right: 2%;
- 
- }
-
-.header {
- width: 100%;
- display: flex;
- justify-content: space-between;
- background-color: #004080;
- color: white;
- padding: 10px 5px 5px 10px;
- margin-bottom: 5%;
- 
- }
-
- 
- .heading {
- font-size: 25;
- 
- }
- 
-</style>
-
-
-
-</html></html>
+</html>
